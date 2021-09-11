523cf6001848e59c10698ae77a5a9627f6b49185
Rafael Trevizoli <rtrevizoli@yahoo.com.br>
2021-09-10 22:16:13 -0300

    ClientController, get and post verbs

--- src/main/java/com/rtx/crm/crmapi/controller/ClientController.java
+++ src/main/java/com/rtx/crm/crmapi/controller/ClientController.java
@@ -1,16 +1,35 @@
 package com.rtx.crm.crmapi.controller;
 
+import java.util.List;
+
+import com.rtx.crm.crmapi.model.Client;
+import com.rtx.crm.crmapi.repository.ClientRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/clients")
 public class ClientController {
 
+    @Autowired
+    private ClientRepository clientRepository;
+
     @GetMapping
-    public String Hello() {
-        return "Hello World";
+    public List<Client> list() {
+        return clientRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Client add(@RequestBody Client client) {
+        return clientRepository.save(client);
     }
 
 }
